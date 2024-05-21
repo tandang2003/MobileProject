01621cc917efff44c0e3a73a2134bf9c9e79dc05
MinhTan <21130171@st.hcmuaf.edu.vn>
2024-05-21 23:13:26 +0700

    update sign up view and login case

--- app/src/main/java/model/User.java
+++ app/src/main/java/model/User.java
@@ -1,2 +1,143 @@
-package model;public class User {
+package model;
+
+import java.util.Objects;
+
+public class User {
+    private int id;
+    private String username;
+    private String avatar;
+    private String password;
+    private String email;
+    //    private String phone;
+//    private String address;
+    private String role;
+    private String status;
+    private String lastSignIn;
+    private String createdAt;
+    private String updatedAt;
+
+    public User() {
+    }
+
+    public User(int id, String username, String avatar, String password, String email, String role, String status, String lastSignIn, String createdAt, String updatedAt) {
+        this.id = id;
+        this.username = username;
+        this.avatar = avatar;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+        this.status = status;
+        this.lastSignIn = lastSignIn;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", avatar='" + avatar + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", role='" + role + '\'' +
+                ", status='" + status + '\'' +
+                ", lastSignIn='" + lastSignIn + '\'' +
+                ", createdAt='" + createdAt + '\'' +
+                ", updatedAt='" + updatedAt + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(username, user.username) && Objects.equals(avatar, user.avatar) && Objects.equals(password, user.password) && Objects.equals(email, user.email) && Objects.equals(role, user.role) && Objects.equals(status, user.status) && Objects.equals(lastSignIn, user.lastSignIn) && Objects.equals(createdAt, user.createdAt) && Objects.equals(updatedAt, user.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, avatar, password, email, role, status, lastSignIn, createdAt, updatedAt);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getLastSignIn() {
+        return lastSignIn;
+    }
+
+    public void setLastSignIn(String lastSignIn) {
+        this.lastSignIn = lastSignIn;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
