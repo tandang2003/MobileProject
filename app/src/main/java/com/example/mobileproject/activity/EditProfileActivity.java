92756cea2f8ee3d5a9dfb8d1001c7dfe2f2fb82b
NguyenHuuTinh1 <21130202@st.hcmuaf.edu.vn>
2024-06-07 21:51:18 +0700

    commit

--- app/src/main/java/com/example/mobileproject/activity/EditProfileActivity.java
+++ app/src/main/java/com/example/mobileproject/activity/EditProfileActivity.java
@@ -1,21 +1,20 @@
 package com.example.mobileproject.fragment;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+
 import com.example.mobileproject.R;
 
-
-public class StoreFragment extends Fragment {
+public class EditProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_store, container, false);
+        return inflater.inflate(R.layout.fragment_editprofile, container, false);
     }
 }
