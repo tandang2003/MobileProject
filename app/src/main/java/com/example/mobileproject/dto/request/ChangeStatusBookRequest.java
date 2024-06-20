064e22d4b5a656a11af64d1c39154da1d98cebcc
MinhTan <21130171@st.hcmuaf.edu.vn>
2024-06-20 18:19:53 +0700

    finish book manager, fixed editUserview, and check role to show manager function

---
+++ app/src/main/java/com/example/mobileproject/dto/request/ChangeStatusBookRequest.java
@@ -0,0 +1,13 @@
+package edu.vn.hcmuaf.ebook.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ChangeStatusBookRequest {
+    Long bookId;
+}
