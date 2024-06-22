d1e31abdf04e78b11cb2a9601cbc492f01a57f1b
MinhTan <21130171@st.hcmuaf.edu.vn>
2024-06-22 17:25:02 +0700

    update .son file

--- app/src/main/java/com/example/mobileproject/api/ApiService.java
+++ app/src/main/java/com/example/mobileproject/api/ApiService.java
@@ -17,7 +17,7 @@ public interface ApiService {
 //            .readTimeout(20, TimeUnit.SECONDS)
             .build();
     Retrofit apiService = new Retrofit.Builder()
-            .baseUrl("http://10.0.2.2:8080/ebook/")
+            .baseUrl("http://10.0.2.16:8080/ebook/")
 //            .client(client)
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build();
